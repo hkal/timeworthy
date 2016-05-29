be2d5e28ff0daf1171c46638778bd88288b2644a
hkal <_@hkal.me>
2016-05-29 00:01:41 -0400

    Cancel process when no command selected

--- scripts/importer.js
+++ scripts/importer.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
+'use strict';
 
 const program = require('commander');
 const request = require('superagent');
 const GameImporter = require('../src/importer/GameImporter');
 
 const importer = new GameImporter();
+let executed = false;
 
 program
   .command('enqueue')
   .action(() => {
+    executed = true;
     request
       .get('http://api.steampowered.com/ISteamApps/GetAppList/v0001/')
       .end((error, response) => {
@@ -23,7 +26,12 @@ program
 program
   .command('process')
   .action(() => {
+    executed = true;
     importer.process();
   });
 
 program.parse(process.argv);
+
+if (!executed) {
+  process.exit(1);
+}
