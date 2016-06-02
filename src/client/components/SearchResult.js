f3c3dd1cd42e89ff4767f633b90cb4656a736a48
hkal <_@hkal.me>
2016-06-02 01:08:17 -0400

    Display discount info to user

--- src/client/components/SearchResult.js
+++ src/client/components/SearchResult.js
@@ -24,12 +24,17 @@ const emphasisStyle = {
   fontWeight: 500
 };
 
+const strikeOutStyle = Object.assign({textDecoration: 'line-through'}, emphasisStyle);
+
 // TODO: Look into using spread operator here
 const priceStyle = Object.assign({color: '#4cae4c'}, emphasisStyle);
 
 class SearchResult extends React.Component {
   render() {
     const game = this.props.game;
+    const priceListItem = (game.salePrice) ?
+      <li>Price: <span style={strikeOutStyle}>{game.priceFormatted}</span> <span style={priceStyle}>{game.salePriceFormatted}</span></li>
+      : <li>Price: <span style={emphasisStyle}>{game.priceFormatted}</span></li>
 
     return (
       <div className='well' style={wellStyle}>
@@ -41,7 +46,7 @@ class SearchResult extends React.Component {
           <div className='col-xs-8'>
             <ul style={priceListStyle}>
               <li>Price per hour: <span style={priceStyle}>{game.pricePerHourFormatted}</span></li>
-              <li>Price: <span style={emphasisStyle}>{game.priceFormatted}</span></li>
+              { priceListItem }
               <li>Time to beat: <span style={emphasisStyle}>{game.mainStoryTimeFormatted}</span></li>
             </ul>
           </div>
